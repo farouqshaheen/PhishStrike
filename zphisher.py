7fc4e6fe2a1b1f1fb0302f00ce9854c5999b63c4
Tamara-Gh <lujienghatashe@gmail.com>
2026-05-07 23:25:01 +0300

    DXFGVBH
    CFVGBHN

--- zphisher.py
+++ zphisher.py
@@ -758,8 +758,8 @@ def main_menu():
     print(f"""
 {RED}[{WHITE}::{RED}]{ORANGE} Select An Attack For Your Victim {RED}[{WHITE}::{RED}]{ORANGE}
 
-{RED}[{WHITE}01{RED}]{ORANGE} Facebook      {RED}[{WHITE}11{RED}]{ORANGE} Twitch       {RED}[{WHITE}21{RED}]{ORANGE} DeviantArt
-{RED}[{WHITE}02{RED}]{ORANGE} Instagram     {RED}[{WHITE}12{RED}]{ORANGE} Pinterest    {RED}[{WHITE}22{RED}]{ORANGE} Badoo
+{RED}[{WHITE}01{RED}]{BLUE} Facebook      {RED}[{WHITE}11{RED}]{ORANGE} Twitch       {RED}[{WHITE}21{RED}]{ORANGE} DeviantArt
+{RED}[{WHITE}02{RED}]{BLUE} Instagram     {RED}[{WHITE}12{RED}]{ORANGE} Pinterest    {RED}[{WHITE}22{RED}]{ORANGE} Badoo
 {RED}[{WHITE}03{RED}]{ORANGE} Google        {RED}[{WHITE}13{RED}]{ORANGE} Snapchat     {RED}[{WHITE}23{RED}]{ORANGE} Origin
 {RED}[{WHITE}04{RED}]{ORANGE} Microsoft     {RED}[{WHITE}14{RED}]{ORANGE} Linkedin     {RED}[{WHITE}24{RED}]{ORANGE} DropBox	
 {RED}[{WHITE}05{RED}]{ORANGE} Netflix       {RED}[{WHITE}15{RED}]{ORANGE} Ebay         {RED}[{WHITE}25{RED}]{ORANGE} Yahoo		
